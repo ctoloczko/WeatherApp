a1480fbec75bb9f19a0b66715f9f56d2c823526f
ctoloczko <celina.toloczko@gmail.com>
2024-04-19 18:05:10 +0200

    Addad js for updating temp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature-value");
+  let temperature = response.data.temperature.current;
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "8eatdeae3d0b8e63a64512c0d2f3a54o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
-  searchFormElement.reset();
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
