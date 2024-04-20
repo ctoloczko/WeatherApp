681d61c22017c5fa06f97842282ddaa34039bdf3
ctoloczko <celina.toloczko@gmail.com>
2024-04-20 14:11:45 +0200

    added display date and time function

--- src/index.js
+++ src/index.js
@@ -7,13 +7,36 @@ function updateWeather(response) {
   );
   let humidityElement = document.querySelector("#weather-app-humidity");
   let windElement = document.querySelector("#weather-app-wind");
+  let dateElement = document.querySelector("#current-date-time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
   windElement.innerHTML = response.data.wind.speed;
-  iconElement.innerHTML = console.log(response.data);
+  dateElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
